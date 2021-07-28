dc572f5054031080bd897f56f1098712bc108d15
Artur Coelho <artur.rocha.coelho@gmail.com>
2021-07-28 10:17:41 -0300

    feat: adicionada a funcionalidade que cadastra a conta poupança

--- src/cadastrar_conta.js
+++ src/cadastrar_conta.js
@@ -1,10 +1,16 @@
-export const signIn = (accountNumber, isBonus) => {
+export const signIn = (accountNumber, isBonus, isSavings) => {
   const account = {
     accountNumber: accountNumber,
     accountBalance: 0,
   };
   if (isBonus) {
     account.accountPoints = 10;
+  }
+  if (isSavings) {
+    account.earnInterest = function (interestRate) {
+      const interestAmount = interestRate * this.accountBalance;
+      this.accountBalance += interestAmount;
+    };
   }
   const textAccount = JSON.stringify(account);
   localStorage.setItem(`${accountNumber}`, textAccount);
