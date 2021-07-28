c2d9c5d3394504008dc4c8987baa6fad2c893eb9
Artur Coelho <artur.rocha.coelho@gmail.com>
2021-07-27 22:39:01 -0300

    Consertado bug do texto exibido em consultar saldo

--- src/cadastrar_conta.js
+++ src/cadastrar_conta.js
@@ -1,8 +1,11 @@
-export const signIn = (accountNumber) => {
+export const signIn = (accountNumber, isBonus) => {
   const account = {
     accountNumber: accountNumber,
     accountBalance: 0,
   };
+  if (isBonus) {
+    account.accountPoints = 10;
+  }
   const textAccount = JSON.stringify(account);
   localStorage.setItem(`${accountNumber}`, textAccount);
 };
--- src/consultar_saldo.js
+++ src/consultar_saldo.js
@@ -1,6 +1,6 @@
 export const consult = (accountNumber, answerLabel) => {
   const response = JSON.parse(localStorage.getItem(`${accountNumber}`));
 
-  answerLabel.innerText += response.accountBalance;
+  answerLabel.innerText = `R$ ${response.accountBalance}`;
   answerLabel.classList.add("show");
 };
